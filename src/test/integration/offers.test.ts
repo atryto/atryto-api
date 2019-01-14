ea3454e2f7894a4cb186cbe33c4d599a8593b32f
marcogbarcellos <marcogbarcellos@gmail.com>
2019-01-14 10:30:58 -0200

    test: fix users test and add offers test

--- src/test/integration/offers.test.ts
+++ src/test/integration/offers.test.ts
@@ -1,197 +1,199 @@
-// import App from "../../app";
-// import {expect} from "chai";
-// import IUser from "../../models/iUser";
-// import "mocha";
-// import UsersService from "../../services/users";
-// import ResetDb from "../resetDb";
-// import { inspect } from "util";
-// import IOffer from "../../models/IOffer";
-// import OffersService from "../../services/offers";
-// import IOfferLookup from "../../models/IOfferLookup";
-// import OfferLookupsService from "../../services/offerLookups";
+import App from "../../app";
+import {expect} from "chai";
+import IUser from "../../models/iUser";
+import "mocha";
+import UsersService from "../../services/users";
+import ResetDb from "../resetDb";
+import { inspect } from "util";
+import IOffer from "../../models/IOffer";
+import OffersService from "../../services/offers";
+import IOfferLookup from "../../models/IOfferLookup";
+import OfferLookupsService from "../../services/offerLookups";
 
-// function encodeObject(obj) {
-//   const esc = encodeURIComponent;
-//   return Object.keys(obj)
-//       .map(k => esc(k) + '=' + esc(obj[k]))
-//       .join('&');
-// }
+function encodeObject(obj) {
+  const esc = encodeURIComponent;
+  return Object.keys(obj)
+      .map(k => esc(k) + '=' + esc(obj[k]))
+      .join('&');
+}
 
-// describe("Offers Endpoints", function() {
-//   const server = new App();
-//   const userService = new UsersService();
-//   const offerService = new OffersService();
-//   const offerLookupService = new OfferLookupsService();
-//   const fastify = server.getFastify();
-//   let user1: IUser, user2: IUser, token1: String, token2: String;
+describe("Offers Endpoints", function() {
+  const server = new App();
+  const userService = new UsersService();
+  const offerService = new OffersService();
+  const offerLookupService = new OfferLookupsService();
+  const fastify = server.getFastify();
+  let user1: IUser, user2: IUser, token1: String, token2: String;
 
-//   before( async () => { 
-//     await ResetDb.run();
-//     // create user 1
-//     await userService.insert({
-//       email: 'useroffer1@test.com',
-//       username: 'useroffer1',
-//       password: 'useroffer1password'
-//     });
-//     // create user 2
-//     await userService.insert({
-//       email: 'useroffer2@test.com',
-//       username: 'useroffer2',
-//       password: 'useroffer2password'
-//     });
-//     // store user1 and user2
-//     let users = await userService.get({email: 'useroffer1@test.com'});
-//     user1 = users[0];
-//     users = await userService.get({email: 'useroffer2@test.com'});
-//     user2 = users[0];
-//     // login to store token of user 1 
-//     token1 =  await userService.login({
-//       email: 'useroffer1@test.com',
-//       password: 'useroffer1password'
-//     });
-//     // login to store token of user 2
-//     token2 =  await userService.login({
-//       email: 'useroffer2@test.com',
-//       password: 'useroffer2password'
-//     });
-//   });
+  before( async () => { 
+    await ResetDb.run();
+    // create user 1
+    await userService.insert({
+      email: 'useroffer1@test.com',
+      username: 'useroffer1',
+      password: 'useroffer1password',
+      cityslug: 'toronto',
+    });
+    // create user 2
+    await userService.insert({
+      email: 'useroffer2@test.com',
+      username: 'useroffer2',
+      password: 'useroffer2password',
+      cityslug: 'toronto',
+    });
+    // store user1 and user2
+    let users = await userService.get({email: 'useroffer1@test.com'});
+    user1 = users[0];
+    users = await userService.get({email: 'useroffer2@test.com'});
+    user2 = users[0];
+    // login to store token of user 1 
+    token1 =  await userService.login({
+      email: 'useroffer1@test.com',
+      password: 'useroffer1password'
+    });
+    // login to store token of user 2
+    token2 =  await userService.login({
+      email: 'useroffer2@test.com',
+      password: 'useroffer2password'
+    });
+  });
 
-//   describe("Testing offer types", function() {
+  describe("Testing offer types", function() {
 
-//     it("Type BUY -> User1 wants to Buy 1500USD with CAD at a rate of 1.30CAD per USD", (done) => {
-//       const offer: IOffer = {
-//         userid: user1.id,
-//         cityslug: 'toronto',
-//         sourcecoinsymbol: 'CAD', // coin the user has
-//         destcoinsymbol: 'USD', // coin the user wanted
-//         wantedpriceperunit: 1.30, // the value of the dest coin in source coin(1 USD  = 1.30 CAD)
-//         amount: 1500,
-//       };
-//       fastify.inject({
-//         method: "POST",
-//         url: "/offers",
-//         headers: {
-//           'x-access-token': token1
-//         },
-//         payload: offer
-//       }, async (err, res) => {
-//         expect(err).to.be.null;
-//         expect(res.statusCode).to.be.equal(200);
-//         expect(res.headers["content-type"]).to.be.equal("application/json");
-//         const offer: IOffer = await offerService.getById(JSON.parse(res.payload).insertId);
-//         expect(offer).to.exist;
-//         expect(offer.userid).to.be.equal(user1.id);
-//         expect(offer.cityslug).to.be.equal('toronto');
-//         expect(offer.sourcecoinsymbol).to.be.equal('CAD');
-//         expect(offer.destcoinsymbol).to.be.equal('USD');
-//         expect(offer.wantedpriceperunit).to.be.equal(1.30);
-//         expect(offer.amount).to.be.equal(1500);
-//         done();
-//       });
-//     });
+    it("Type BUY -> User1 wants to Buy 1500USD with CAD at a rate of 1.30CAD per USD", (done) => {
+      const offer: IOffer = {
+        userid: user1.id,
+        cityslug: 'toronto',
+        sourcecoinsymbol: 'CAD', // coin the user has
+        destcoinsymbol: 'USD', // coin the user wanted
+        wantedpriceperunit: 1.30, // the value of the dest coin in source coin(1 USD  = 1.30 CAD)
+        amount: 1500,
+      };
+      fastify.inject({
+        method: "POST",
+        url: "/offers",
+        headers: {
+          'x-access-token': token1
+        },
+        payload: offer
+      }, async (err, res) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.headers["content-type"]).to.be.equal("application/json");
+        const offer: IOffer = await offerService.getById(JSON.parse(res.payload).insertId);
+        expect(offer).to.exist;
+        expect(offer.userid).to.be.equal(user1.id);
+        expect(offer.cityslug).to.be.equal('toronto');
+        expect(offer.sourcecoinsymbol).to.be.equal('CAD');
+        expect(offer.destcoinsymbol).to.be.equal('USD');
+        expect(offer.wantedpriceperunit).to.be.equal(1.30);
+        expect(offer.amount).to.be.equal(1500);
+        done();
+      });
+    });
 
-//     it("Type SELL -> User1 wants to Sell 500CAD for USD at a rate of 1.30 CAD per usd", (done) => {
-//       const offer: IOffer = {
-//         userid: user1.id,
-//         cityslug: 'toronto',
-//         sourcecoinsymbol: 'CAD', // coin the user has
-//         destcoinsymbol: 'USD', // coin the user wants
-//         wantedpriceperunit: 1.30, // the value of the dest coin in source coin(1 USD  = 1.30 CAD)
-//         amount: 500,
-//       };
-//       fastify.inject({
-//         method: "POST",
-//         url: "/offers",
-//         headers: {
-//           'x-access-token': token1
-//         },
-//         payload: offer
-//       }, async (err, res) => {
-//         expect(err).to.be.null;
-//         expect(res.statusCode).to.be.equal(200);
-//         expect(res.headers["content-type"]).to.be.equal("application/json");
-//         const offer: IOffer = await offerService.getById(JSON.parse(res.payload).insertId);
-//         expect(offer).to.exist;
-//         expect(offer.userid).to.be.equal(user1.id);
-//         expect(offer.cityslug).to.be.equal('toronto');
-//         expect(offer.sourcecoinsymbol).to.be.equal('CAD');
-//         expect(offer.destcoinsymbol).to.be.equal('USD');
-//         expect(offer.wantedpriceperunit).to.be.equal(1.30);
-//         expect(offer.amount).to.be.equal(500);
-//         done();
-//       });
-//     });
+    it("Type SELL -> User1 wants to Sell 500CAD for USD at a rate of 1.30 CAD per usd", (done) => {
+      const offer: IOffer = {
+        userid: user1.id,
+        cityslug: 'toronto',
+        sourcecoinsymbol: 'CAD', // coin the user has
+        destcoinsymbol: 'USD', // coin the user wants
+        wantedpriceperunit: 1.30, // the value of the dest coin in source coin(1 USD  = 1.30 CAD)
+        amount: 500,
+      };
+      fastify.inject({
+        method: "POST",
+        url: "/offers",
+        headers: {
+          'x-access-token': token1
+        },
+        payload: offer
+      }, async (err, res) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.headers["content-type"]).to.be.equal("application/json");
+        const offer: IOffer = await offerService.getById(JSON.parse(res.payload).insertId);
+        expect(offer).to.exist;
+        expect(offer.userid).to.be.equal(user1.id);
+        expect(offer.cityslug).to.be.equal('toronto');
+        expect(offer.sourcecoinsymbol).to.be.equal('CAD');
+        expect(offer.destcoinsymbol).to.be.equal('USD');
+        expect(offer.wantedpriceperunit).to.be.equal(1.30);
+        expect(offer.amount).to.be.equal(500);
+        done();
+      });
+    });
 
-//     it("Type EXCHANGE -> User1 wants to EXCHANGE 4BTC for ETH at a rate of 7.5ETH per BTC", (done) => {
-//       const offer: IOffer = {
-//         userid: user1.id,
-//         cityslug: 'toronto',
-//         sourcecoinsymbol: 'BTC', // coin the user has
-//         destcoinsymbol: 'ETH', // coin the user wants
-//         wantedpriceperunit: 7.5, // the value of the dest coin in source coin(1 BTC = 7.5 ETH)
-//         amount: 4,
-//       };
-//       fastify.inject({
-//         method: "POST",
-//         url: "/offers",
-//         headers: {
-//           'x-access-token': token1
-//         },
-//         payload: offer
-//       }, async (err, res) => {
-//         expect(err).to.be.null;
-//         expect(res.statusCode).to.be.equal(200);
-//         expect(res.headers["content-type"]).to.be.equal("application/json");
-//         const offer: IOffer = await offerService.getById(JSON.parse(res.payload).insertId);
-//         expect(offer).to.exist;
-//         expect(offer.userid).to.be.equal(user1.id);
-//         expect(offer.cityslug).to.be.equal('toronto');
-//         expect(offer.sourcecoinsymbol).to.be.equal('BTC');
-//         expect(offer.destcoinsymbol).to.be.equal('ETH');
-//         expect(offer.wantedpriceperunit).to.be.equal(7.5);
-//         expect(offer.amount).to.be.equal(4);
-//         done();
-//       });
-//     });
+    it("Type EXCHANGE -> User1 wants to EXCHANGE 4BTC for ETH at a rate of 7.5ETH per BTC", (done) => {
+      const offer: IOffer = {
+        userid: user1.id,
+        cityslug: 'toronto',
+        sourcecoinsymbol: 'BTC', // coin the user has
+        destcoinsymbol: 'ETH', // coin the user wants
+        wantedpriceperunit: 7.5, // the value of the dest coin in source coin(1 BTC = 7.5 ETH)
+        amount: 4,
+      };
+      fastify.inject({
+        method: "POST",
+        url: "/offers",
+        headers: {
+          'x-access-token': token1
+        },
+        payload: offer
+      }, async (err, res) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.headers["content-type"]).to.be.equal("application/json");
+        const offer: IOffer = await offerService.getById(JSON.parse(res.payload).insertId);
+        expect(offer).to.exist;
+        expect(offer.userid).to.be.equal(user1.id);
+        expect(offer.cityslug).to.be.equal('toronto');
+        expect(offer.sourcecoinsymbol).to.be.equal('BTC');
+        expect(offer.destcoinsymbol).to.be.equal('ETH');
+        expect(offer.wantedpriceperunit).to.be.equal(7.5);
+        expect(offer.amount).to.be.equal(4);
+        done();
+      });
+    });
 
-//   });
+  });
 
-//   describe("Testing offer lookups", function() {
-//     it("Whenever someone looks for a coin in the platform we add that lookup", (done) => {
-//       const lookup: IOfferLookup = {
-//         cityslug: 'toronto',
-//         sourcecoinsymbol: 'BTC', // coin the user has
-//         destcoinsymbol: 'ETH', // coin the user wants
-//       };
-//       const queryOffer = encodeObject(lookup);
-//       console.log(`queryOffer: ${queryOffer}`);
-//       fastify.inject({
-//         method: "GET",
-//         url: "/offers?"+queryOffer,
-//       }, async (err, res) => {
-//         expect(err).to.be.null;
-//         expect(res.statusCode).to.be.equal(200);
-//         expect(res.headers["content-type"]).to.be.equal("application/json");
-//         const offers = JSON.parse(res.payload).result;
-//         expect(offers).to.exist;
-//         console.log(`offers: ${JSON.stringify(offers, null, 2)}`);
-//         expect(offers).to.have.length.above(0);
+  describe("Testing offer lookups", function() {
+    it("Whenever someone looks for a coin in the platform we add that lookup", (done) => {
+      const lookup: IOfferLookup = {
+        cityslug: 'toronto',
+        sourcecoinsymbol: 'BTC', // coin the user has
+        destcoinsymbol: 'ETH', // coin the user wants
+      };
+      const queryOffer = encodeObject(lookup);
+      console.log(`queryOffer: ${queryOffer}`);
+      fastify.inject({
+        method: "GET",
+        url: "/offers?"+queryOffer,
+      }, async (err, res) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.headers["content-type"]).to.be.equal("application/json");
+        const offers = JSON.parse(res.payload).result;
+        expect(offers).to.exist;
+        console.log(`offers: ${JSON.stringify(offers, null, 2)}`);
+        expect(offers).to.have.length.above(0);
 
-//         let offerLookups: IOfferLookup[] = [];
-//         // OfferLookup is executed asynchronously so we wait
-//         while (offerLookups.length == 0 ) {
-//           offerLookups = await offerLookupService.get(lookup);
-//         }
-//         console.log(`offerLookups: ${JSON.stringify(offerLookups, null, 2)}`);
-//         expect(offerLookups).to.not.be.empty;
-//         const offerLookup: IOfferLookup = offerLookups[0];
-//         console.log(`offerLookup: ${JSON.stringify(offerLookup, null, 2)}`);
-//         expect(offerLookup.cityslug).to.be.equal(lookup.cityslug);
-//         expect(offerLookup.sourcecoinsymbol).to.be.equal(lookup.sourcecoinsymbol);
-//         expect(offerLookup.destcoinsymbol).to.be.equal(lookup.destcoinsymbol);
-//         done();
-//       });
-//     });
-//   });
+        let offerLookups: IOfferLookup[] = [];
+        // OfferLookup is executed asynchronously so we wait
+        while (offerLookups.length == 0 ) {
+          offerLookups = await offerLookupService.get(lookup);
+        }
+        console.log(`offerLookups: ${JSON.stringify(offerLookups, null, 2)}`);
+        expect(offerLookups).to.not.be.empty;
+        const offerLookup: IOfferLookup = offerLookups[0];
+        console.log(`offerLookup: ${JSON.stringify(offerLookup, null, 2)}`);
+        expect(offerLookup.cityslug).to.be.equal(lookup.cityslug);
+        expect(offerLookup.sourcecoinsymbol).to.be.equal(lookup.sourcecoinsymbol);
+        expect(offerLookup.destcoinsymbol).to.be.equal(lookup.destcoinsymbol);
+        done();
+      });
+    });
+  });
 
-// });
+});
--- src/test/integration/users.test.ts
+++ src/test/integration/users.test.ts
@@ -23,182 +23,183 @@ describe("Users Endpoints", function() {
 
   before( async () => { await ResetDb.run() });
 
-  it("POST /users Should fail because there is no cityslug defined for user", (done) => {
-    fastify.inject({
-      method: "POST",
-      url: "/users",
-      payload: {
-        email: 'user@test.com',
-        username: 'user1',
-        password: 'test'
-      },
-    }, async (err, res) => {
-      // expect(err).to.not.be.null;
-      // expect(err.msg).to.exist;
-      // 'ER_NO_DEFAULT_FOR_FIELD'
-      console.log(`\n\n\n\n\n\n\n\n\nres.statusCode: ${res.statusCode}\n\n\n\n\n\n\n\n\n`);
-      console.log(`\n\n\n\n\n\n\n\n\nerr: ${JSON.stringify(res.payload)}\n\n\n\n\n\n\n\n\n`);
-      expect(res.statusCode).to.be.equal(500);
-      expect(res.payload).to.exist;
-      expect(JSON.parse(res.payload).message).to.exist;
-      expect(JSON.parse(res.payload).message).to.contain('ER_NO_DEFAULT_FOR_FIELD: Field \'cityslug\'');
-      done();
+  describe("POST /users", function() {
+    it("Should fail because there is no cityslug defined for user", (done) => {
+      fastify.inject({
+        method: "POST",
+        url: "/users",
+        payload: {
+          email: 'user@test.com',
+          username: 'user1',
+          password: 'test'
+        },
+      }, async (err, res) => {
+        expect(res.statusCode).to.be.equal(500);
+        expect(res.payload).to.exist;
+        expect(JSON.parse(res.payload).message).to.exist;
+        expect(JSON.parse(res.payload).message).to.contain('ER_NO_DEFAULT_FOR_FIELD: Field \'cityslug\'');
+        done();
+      });
     });
-  });
-
-  it("POST /users Should create a user", (done) => {
-    fastify.inject({
-      method: "POST",
-      url: "/users",
-      payload: {
-        email: 'user@test.com',
-        username: 'user1',
-        password: 'test',
-        cityslug: 'toronto',
-      },
-    }, async (err, res) => {
-      expect(err).to.be.null;
-      expect(res.statusCode).to.be.equal(200);
-      expect(res.headers["content-type"]).to.be.equal("application/json");
-      const users = await service.get({} as IUser);
-      expect(users).to.have.lengthOf(1);
-      user = users[0];
-      expect(user.email).to.be.equal('user@test.com');
-      expect(user.username).to.be.equal('user1');
-      expect(user.password).to.not.exist;
-      done();
-    });
-  });
-
-  it("POST /users Should not allow SQL Injection", (done) => {
-    fastify.inject({
-      method: "POST",
-      url: "/users",
-      payload: {
-        email: 'user2@test.com',
-        username: 'user2; DELETE FROM exchanges.users;',
-        password: 'test',
-        cityslug: 'toronto',
-      },
-    }, async (err, res) => {
-      expect(err).to.be.null;
-      expect(res.statusCode).to.be.equal(200);
-      expect(res.headers["content-type"]).to.be.equal("application/json");
-      const users = await service.get({} as IUser);
-      expect(users).to.have.lengthOf(2);
-      const notInjectionUser = users[1];
-      expect(notInjectionUser.username).to.be.equal('user2; DELETE FROM exchanges.users;');
-      done();
-    });
-  });
-
-  it("POST /users/login Should login user and return token", (done) => {
-    fastify.inject({
-      method: "POST",
-      url: "/users/login",
-      payload: {
-        email: 'user@test.com',
-        password: 'test'
-      },
-    }, async (err, res) => {
-      expect(err).to.be.null;
-      expect(res.statusCode).to.be.equal(200);
-      expect(res.headers["content-type"]).to.be.equal("application/json");
-      const tokenData = JSON.parse(res.payload);
-      expect(tokenData).to.have.property('token');
-      token = tokenData.token;
-      done();
-    });
-  });
   
-
-  it("PUT /users Should fail without token", (done) => {
-    fastify.inject({
-      method: "PUT",
-      url: `/users/${user.id}`,
-    }, async (err, res) => {
-      expect(err).to.be.null;
-      expect(res.statusCode).to.be.equal(401);
-      expect(res.headers["content-type"]).to.be.equal("application/json");
-      expect(res.payload).to.exist;
-      expect(JSON.parse(res.payload).message).to.exist;
-      expect(JSON.parse(res.payload).message).to.contain('token provided');
-      done();
+    it("Should create a user", (done) => {
+      fastify.inject({
+        method: "POST",
+        url: "/users",
+        payload: {
+          email: 'user@test.com',
+          username: 'user1',
+          password: 'test',
+          cityslug: 'toronto',
+        },
+      }, async (err, res) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.headers["content-type"]).to.be.equal("application/json");
+        const users = await service.get({} as IUser);
+        expect(users).to.have.lengthOf(1);
+        user = users[0];
+        expect(user.email).to.be.equal('user@test.com');
+        expect(user.username).to.be.equal('user1');
+        expect(user.password).to.not.exist;
+        done();
+      });
+    });
+  
+    it("Should not allow SQL Injection", (done) => {
+      fastify.inject({
+        method: "POST",
+        url: "/users",
+        payload: {
+          email: 'user2@test.com',
+          username: 'user2; DELETE FROM exchanges.users;',
+          password: 'test',
+          cityslug: 'toronto',
+        },
+      }, async (err, res) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.headers["content-type"]).to.be.equal("application/json");
+        const users = await service.get({} as IUser);
+        expect(users).to.have.lengthOf(2);
+        const notInjectionUser = users[1];
+        expect(notInjectionUser.username).to.be.equal('user2; DELETE FROM exchanges.users;');
+        done();
+      });
     });
   });
 
-  it("PUT /users Should should fail if try to modify email", (done) => {
-    fastify.inject({
-      method: "PUT",
-      url: `/users/${user.id}`,
-      headers: {
-        'x-access-token': token
-      },
-      payload: {
-        email: 'new_email@gmail.com',
-      },
-    }, async (err, res) => {
-      expect(res.statusCode).to.be.equal(500);
-      expect(res.payload).to.exist;
-      expect(JSON.parse(res.payload).message).to.exist;
-      expect(JSON.parse(res.payload).message).to.contain('email cannot be modified');
-      done();
+  describe("POST /users/login", function() {
+    it("Should login user and return token", (done) => {
+      fastify.inject({
+        method: "POST",
+        url: "/users/login",
+        payload: {
+          email: 'user@test.com',
+          password: 'test'
+        },
+      }, async (err, res) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.headers["content-type"]).to.be.equal("application/json");
+        const tokenData = JSON.parse(res.payload);
+        expect(tokenData).to.have.property('token');
+        token = tokenData.token;
+        done();
+      });
+    });
+  });  
+  describe("PUT /users", function() {
+    it("Should fail without token", (done) => {
+      fastify.inject({
+        method: "PUT",
+        url: `/users/${user.id}`,
+      }, async (err, res) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.be.equal(401);
+        expect(res.headers["content-type"]).to.be.equal("application/json");
+        expect(res.payload).to.exist;
+        expect(JSON.parse(res.payload).message).to.exist;
+        expect(JSON.parse(res.payload).message).to.contain('token provided');
+        done();
+      });
+    });
+  
+    it("Should should fail if try to modify email", (done) => {
+      fastify.inject({
+        method: "PUT",
+        url: `/users/${user.id}`,
+        headers: {
+          'x-access-token': token
+        },
+        payload: {
+          email: 'new_email@gmail.com',
+        },
+      }, async (err, res) => {
+        expect(res.statusCode).to.be.equal(500);
+        expect(res.payload).to.exist;
+        expect(JSON.parse(res.payload).message).to.exist;
+        expect(JSON.parse(res.payload).message).to.contain('email cannot be modified');
+        done();
+      });
+    });
+  
+    it("Should should fail if try to modify username", (done) => {
+      fastify.inject({
+        method: "PUT",
+        url: `/users/${user.id}`,
+        headers: {
+          'x-access-token': token
+        },
+        payload: {
+          username: 'new_name',
+        },
+      }, async (err, res) => {
+        expect(res.statusCode).to.be.equal(500);
+        expect(res.payload).to.exist;
+        expect(JSON.parse(res.payload).message).to.exist;
+        expect(JSON.parse(res.payload).message).to.contain('username cannot be modified');
+        done();
+      });
+    });
+    
+    it("Should be authenticated with token", (done) => {
+      fastify.inject({
+        method: "PUT",
+        url: `/users/${user.id}`,
+        headers: {
+          'x-access-token': token
+        },
+        payload: {
+          cityslug: 'vancouver',
+        },
+      }, async (err, res) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.be.equal(200);
+        const updatedUser = await dao.getById(user.id);
+        expect(user.cityslug).to.not.equal(updatedUser.cityslug);
+        expect(res.headers["content-type"]).to.be.equal("application/json");
+        done();
+      });
     });
   });
 
-  it("PUT /users Should should fail if try to modify username", (done) => {
-    fastify.inject({
-      method: "PUT",
-      url: `/users/${user.id}`,
-      headers: {
-        'x-access-token': token
-      },
-      payload: {
-        username: 'new_name',
-      },
-    }, async (err, res) => {
-      expect(res.statusCode).to.be.equal(500);
-      expect(res.payload).to.exist;
-      expect(JSON.parse(res.payload).message).to.exist;
-      expect(JSON.parse(res.payload).message).to.contain('username cannot be modified');
-      done();
-    });
-  });
-  // Put password should be encrypted if changed
-  // should fail if try to modify username
-  it("PUT /users Should be authenticated with token", (done) => {
-    fastify.inject({
-      method: "PUT",
-      url: `/users/${user.id}`,
-      headers: {
-        'x-access-token': token
-      },
-      payload: {
-        cityslug: 'vancouver',
-      },
-    }, async (err, res) => {
-      expect(err).to.be.null;
-      expect(res.statusCode).to.be.equal(200);
-      const updatedUser = await dao.getById(user.id);
-      expect(user.cityslug).to.not.equal(updatedUser.cityslug);
-      expect(res.headers["content-type"]).to.be.equal("application/json");
-      done();
-    });
-  });
 
-  it("GET /users Should return 2 users", (done) => {
-    fastify.inject({
-      method: "GET",
-      url: "/users"
-    }, async (err, res) => {
-      expect(err).to.be.null;
-      expect(res.statusCode).to.be.equal(200);
-      expect(res.headers["content-type"]).to.be.equal("application/json");
-      expect(res.payload).to.exist;
-      const users = JSON.parse(res.payload).result;
-      expect(users).to.exist;
-      expect(users).to.have.lengthOf(2);
-      done();
+  describe("GET /users", function() {
+    it("Should return 2 users", (done) => {
+      fastify.inject({
+        method: "GET",
+        url: "/users"
+      }, async (err, res) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.headers["content-type"]).to.be.equal("application/json");
+        expect(res.payload).to.exist;
+        const users = JSON.parse(res.payload).result;
+        expect(users).to.exist;
+        expect(users).to.have.lengthOf(2);
+        done();
+      });
     });
   });
 
