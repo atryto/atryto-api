070abf770ea1248c245f1c017be3f015ad135589
marcogbarcellos <marcogbarcellos@gmail.com>
2019-01-24 00:34:29 -0200

    feat(services/icrudservice): add parameters to get and insert methods of ICrudService interface

--- src/services/iCrudService.ts
+++ src/services/iCrudService.ts
@@ -1,8 +1,8 @@
 import { Model } from "sequelize-typescript";
 
 export default interface ICrudService<T extends Model<T>> {
-  insert(model: any): Promise<T>;
-  get(model: any): Promise<T[]>;
+  insert(where: any): Promise<T>;
+  get(where: any, limit: number, offset: number, attributes: string[], order: any): Promise<T[]>;
   update(id:number, model: any): Promise<T>;
   getById(id: number): Promise<T>;
   delete(id: number): Promise<Boolean>;
